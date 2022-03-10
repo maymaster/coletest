7d74d2219498bf8acf0d1bcb373848b7acabf171
Mario Ivan Gonzalez Barillas <li-mgonzalez@hotmail.com>
2022-03-10 11:38:32 -0600

    Blog: Few Quick Edits & Improvements

--- front-page.php
+++ front-page.php
@@ -67,7 +67,7 @@
             <?php }
             ?>
 
-            <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+            <p class="t-center no-margin"><a href="<?php echo site_url('/blog')?>" class="btn btn--yellow">View All Blog Posts</a></p>
         </div>
     </div>
 </div>

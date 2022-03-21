1506998beca059cdce401345bf7632f0b7106253
Mario Ivan Gonzalez Barillas <li-mgonzalez@hotmail.com>
2022-03-21 15:13:35 -0600

    37. Quick Program Edits

--- front-page.php
+++ front-page.php
@@ -6,7 +6,8 @@
         <h1 class="headline headline--large">Welcome! <?php echo basename($template); ?> </h1>
         <h2 class="headline headline--medium">We think you&rsquo;ll like it here.</h2>
         <h3 class="headline headline--small">Why don&rsquo;t you check out the <strong>major</strong> you&rsquo;re interested in?</h3>
-        <a href="#" class="btn btn--large btn--blue">Find Your Major</a>
+        <a href="<?php echo get_post_type_archive_link('program'); ?>" class="btn btn--large btn--blue">Find Your Major</a>
+
     </div>
 </div>
 
@@ -37,7 +38,7 @@
             while ($homepageEvents->have_posts()) {
                 $homepageEvents->the_post(); ?>
                 <div class="event-summary">
-                    <a class="event-summary__date t-center" href="#">
+                    <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
                         <span class="event-summary__month"><?php
                                                             $eventDate = new DateTime(get_field('event_date'));
                                                             echo $eventDate->format('M')
